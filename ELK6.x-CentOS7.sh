f1b4cdbcf2206967a8dfbd1b004ac34219d25c4f
Tuxtter <tuxtter@gmail.com>
2019-02-15 14:55:15 -0500

    Update ELK6.x-CentOS7.sh

--- ELK6.x-CentOS7.sh
+++ ELK6.x-CentOS7.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 yum update -y
 yum upgrade -y
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.rpm"
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jre-8u172-linux-x64.rpm"
-rpm -ivh jdk-8u172-linux-x64.rpm
-rpm -ivh jre-8u172-linux-x64.rpm
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
-echo "[logstash-6.x]
-name=Elastic repository for 6.x packages
+cat > /etc/yum.repos.d/elasticsearch.repo << EOF
+[elasticsearch-6.x]
+name=Elasticsearch repository for 6.x packages
 baseurl=https://artifacts.elastic.co/packages/6.x/yum
 gpgcheck=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
 autorefresh=1
-type=rpm-md" > /etc/yum.repos.d/elasticsearch.repo
+type=rpm-md
+EOF
+curl -Lo jre-8-linux-x64.rpm --header "Cookie: oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn-pub/java/jdk/8u202-b08/1961070e4c9b4e26a04e7f5a083f551e/jre-8u202-linux-x64.rpm"
+curl -Lo jdk-8-linux-x64.rpm --header "Cookie: oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn-pub/java/jdk/8u202-b08/1961070e4c9b4e26a04e7f5a083f551e/jdk-8u202-linux-x64.rpm"
+rpm -qlp jre-8-linux-x64.rpm > /dev/null 2>&1 && echo "Java package downloaded successfully" || echo "Java package did not download successfully"
+yum -y install jre-8-linux-x64.rpm
+rm -f jre-8-linux-x64.rpm
+rpm -qlp jdk-8-linux-x64.rpm > /dev/null 2>&1 && echo "Java package downloaded successfully" || echo "Java package did not download successfully"
+yum -y install jdk-8-linux-x64.rpm
+rm -f jdk-8-linux-x64.rpm
 yum update -y
-yum -y install elasticsearch kibana logstash
-chkconfig --add kibana
-chkconfig --add logstash
-chkconfig --add elasticsearch
-chkconfig logstash on
-chkconfig elasticsearch on
-chkconfig kibana on
+yum -y install elasticsearch logstash
+systemctl daemon-reload
+systemctl enable elasticsearch.service
+chmod g+w /etc/elasticsearch
+usermod -a -G ossec logstash
+systemctl daemon-reload
+systemctl enable logstash.service
+wget https://artifacts.elastic.co/downloads/kibana/kibana-oss-6.6.0-x86_64.rpm
+rpm -iv kibana-oss-6.6.0-x86_64.rpm
 /usr/share/logstash/bin/logstash-plugin install logstash-output-email
 echo "cluster.name: elk01
 node.name: elk01-nodo01
@@ -34,17 +42,19 @@ CONF_DIR=/etc/elasticsearch
 DATA_DIR=/var/lib/elasticsearch
 LOG_DIR=/var/log/elasticsearch
 PID_DIR=/var/run/elasticsearch
-ES_USER=elasticsearch
-ES_GROUP=elasticsearch
 ES_STARTUP_SLEEP_TIME=5
 MAX_OPEN_FILES=9965536" >> /etc/sysconfig/elasticsearch
+rm -rf /usr/share/kibana/optimize/bundles
 echo 'server.port: 5601
-server.host: "localhost"
-server.name: "ELK06"
+server.host: "0.0.0.0
+server.name: "ELK01
 elasticsearch.url: "http://localhost:9200"
 elasticsearch.preserveHost: true
 kibana.index: ".kibana"
 kibana.defaultAppId: "discover"' >> /etc/kibana/kibana.yml
-service elasticsearch start
-service logstash start
-service kibana start
+systemctl start elasticsearch.service
+systemctl start logstash.service
+systemctl daemon-reload
+systemctl enable kibana.service
+systemctl start kibana.service
+sed -i "s/^enabled=1/enabled=0/" /etc/yum.repos.d/elasticsearch.repo
